5dee462b4a0b9b70f47210101c9648e3bade38d3
5h4k4r <shakar.halabjai@yahoo.com>
2021-04-07 16:13:19 +0300

    updated menus in pages menu

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -3,34 +3,46 @@ import { NbMenuItem } from '@nebular/theme';
 export const MENU_ITEMS: NbMenuItem[] = [
   {
     title: 'Dashboard',
-    icon: 'home-outline',
-    link: '/pages/dashboard',
+    icon: { icon: 'table', pack: 'solid' },
+    link: '/dashboard',
     home: true,
   },
   {
-    title: 'FEATURES',
+    title: 'MANAGEMENT',
     group: true,
   },
   {
-    title: 'Auth',
-    icon: 'lock-outline',
-    children: [
-      {
-        title: 'Login',
-        link: '/auth/login',
-      },
-      {
-        title: 'Register',
-        link: '/auth/register',
-      },
-      {
-        title: 'Request Password',
-        link: '/auth/request-password',
-      },
-      {
-        title: 'Reset Password',
-        link: '/auth/reset-password',
-      },
-    ],
+    title: 'Admins',
+    icon: 'user',
+    link: '/dashboard/users/admin',
+  },
+  {
+    title: 'Drivers',
+    icon: 'dot-circle',
+    link: '/dashboard/users/driver',
+  },
+  {
+    title: 'Orders',
+    icon: 'file-alt',
+    link: '/dashboard/orders',
+  },
+  {
+    title: 'SETTINGS',
+    group: true,
+  },
+  {
+    title: 'Company',
+    icon: 'building',
+    link: '/dashboard/settings/company',
+  },
+  {
+    title: 'Subscription',
+    icon: 'credit-card',
+    link: '/dashboard/settings/subscription',
+  },
+  {
+    title: 'API Keys',
+    icon: { icon: 'key', pack: 'solid' },
+    link: '/dashboard/settings/api-keys',
   },
 ];
